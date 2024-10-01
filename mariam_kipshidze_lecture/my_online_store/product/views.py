d5404f24fe1fdb8ffd0527bcde1de7d357af5461
Mariam <mari.kifshidze@gmail.com>
2024-10-02 00:00:33 +0400

    Initial Project

---
+++ mariam_kipshidze_lecture/my_online_store/product/views.py
@@ -0,0 +1,40 @@
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
+
+from product.models import Product
+from product.serializers import ProductSerializer, MutateProductSerializer, CreateProductSerializer, \
+    ProductDynamicFieldsSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+
+    def get_serializer(self, *args, **kwargs):
+        if self.action == 'list':
+            return ProductDynamicFieldsSerializer(*args, **kwargs, fields=['id', 'title'])
+        elif self.action == 'retrieve':
+            return ProductDynamicFieldsSerializer(
+                *args, **kwargs, fields=['id', 'title', 'categories', 'price', 'tag'])
+        elif self.action == 'create':
+            return CreateProductSerializer(*args, **kwargs)
+        return MutateProductSerializer(*args, **kwargs)
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAuthenticated()]
+        return super().get_permissions()
+
+    def perform_destroy(self, instance):
+        instance.is_deleted = True
+        instance.save()
+
+
+class ProductCreateListDetailViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
---
+++ mariam_kipshidze_lecture/my_online_store/user/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from user.forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully')
+            return redirect('home')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'register.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
+
+def logout(request):
+    auth_logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect('login')
+
+def home(request):
+    return render(request, 'home.html')
