1cf7b8e4a86925f35d5b1648031fde5138f2082d
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-10-06 14:23:55 +0400

    add permission on users

--- nikoloz_mdinaradze_lecture/users/views.py
+++ nikoloz_mdinaradze_lecture/users/views.py
@@ -3,9 +3,13 @@ from rest_framework import viewsets
 
 from users.serializers import UserSerializer
 from users.filters import UserFilter
+from users.permissions import IsAuthenticatedUser
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     filterset_class = UserFilter
+    permission_classes = [IsAuthenticatedUser]
+
 
