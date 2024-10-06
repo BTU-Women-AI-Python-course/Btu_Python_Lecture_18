5603885f2d4570759f0c91d25d7f84e60cc9ea63
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-10-06 14:09:34 +0400

    add django filters on users

--- nikoloz_mdinaradze_lecture/users/views.py
+++ nikoloz_mdinaradze_lecture/users/views.py
@@ -2,8 +2,10 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 
 from users.serializers import UserSerializer
-
+from users.filters import UserFilter
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filterset_class = UserFilter
+
