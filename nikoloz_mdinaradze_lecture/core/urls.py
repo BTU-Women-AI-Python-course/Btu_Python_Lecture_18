b243cb1cd8540088b5633e9e57392aa3aacbc678
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-10-06 13:48:49 +0400

    add users viewset with global pagination

--- nikoloz_mdinaradze_lecture/core/urls.py
+++ nikoloz_mdinaradze_lecture/core/urls.py
@@ -32,4 +32,7 @@ urlpatterns = [
     # JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    ]
+
+    # My URL
+    path('users/', include('users.urls')),
+]
---
+++ nikoloz_mdinaradze_lecture/users/urls.py
@@ -0,0 +1,7 @@
+from rest_framework import routers
+
+from users.views import UserViewSet
+
+router = routers.SimpleRouter()
+router.register('', UserViewSet)
+urlpatterns = router.urls
